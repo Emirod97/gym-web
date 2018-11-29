c1da0a83f44a982eb2abd82c8b8540dd2428740b
IsabelZR <izavala@cetys.edu.mx>
2018-11-28 21:21:54 -0800

    y esto también

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { FilterComponent } from './filter/filter.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserRegisterComponent,
+    UsersListComponent,
+    FilterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
