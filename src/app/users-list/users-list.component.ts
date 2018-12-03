db2b8a5ccdb02e0f7510375508ca37f6180c9528
José Luis Castro <jluis.scooter11@hotmail.com>
2018-12-03 12:46:52 -0800

    Attendances list with filters functionality

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,15 +1,89 @@
+import { Service } from './../../services/Service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-users-list',
-  templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+    selector: 'app-users-list',
+    templateUrl: './users-list.component.html',
+    styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+    members;
+    attendances;
+    studentNumber: number;
+    date;
+    isFilters: boolean = false;
 
-  ngOnInit() {
-  }
+    constructor(public service: Service) {
+    }
+
+    ngOnInit() {
+        this.getAllAttendances();
+    }
+
+    getAllAttendances() {
+        this.service.getAttendances().subscribe(
+            (data) => {
+              this.setAttendances(data);
+              this.clearFields();
+            },
+            error => console.log(error)
+        );
+    }
+
+    setAttendances(attendances) {
+        this.attendances = attendances;
+        console.log(this.attendances);
+    }
+
+    filter() {
+        console.log("date: ", this.date);
+        console.log("studentNumber: ", this.studentNumber);
+
+        if (!this.studentNumber && !this.date) {
+            this.getAllAttendances();
+        }
+
+        else if (this.studentNumber && !this.date) {
+            this.service.getAttendancesByMemberId(this.studentNumber).subscribe(
+                (data) => {
+                  this.setAttendances(data);
+                  this.isFilters = true;
+                },
+                (error) => console.log(error)
+            )
+        }
+
+        else if (!this.studentNumber && this.date){
+            this.service.getAttendancesByDate(this.date).subscribe(
+              (data)=> {
+                this.setAttendances(data);
+                this.isFilters = true;
+              },
+              (error)=> console.log(error)
+            )    
+        }
+
+        else if(this.studentNumber && this.date){
+          this.service.getAttendancesByMemberIdAndDate(this.studentNumber, this.date).subscribe(
+            (data) => {
+              this.setAttendances(data);
+              this.isFilters = true;
+            },
+            (error) => console.log(error)
+          )
+        }
+    }
+
+    removeFilters(){
+      this.getAllAttendances();
+      this.clearFields();
+    }
+
+    clearFields(){
+      this.isFilters = false;   
+      this.date = undefined;
+      this.studentNumber = undefined;
+    }
 
 }
